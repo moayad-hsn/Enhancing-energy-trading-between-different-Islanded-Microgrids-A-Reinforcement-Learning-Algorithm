eaa8be33973618479be24d395dbe151d154ebabb
FayElhassan <38003091+FayElhassan@users.noreply.github.com>
2020-07-30 08:05:45 +0200

    August Day 1 Data

--- Final Version ISA/inverter.m
+++ Final Version ISA/inverter.m
@@ -1,9 +1,9 @@
-data=readtable("Day1.csv");
+data=readtable("Day2.csv");
 
-Tss=5e-6;
+Tss=2.5e-6;
 
-P=1.7e6;
-U=380;
+P=425000;
+U=220;
 f=50;
 fsw=5e3;
 
@@ -15,9 +15,9 @@ timeStamp = data.TimeStamp;
 irrad = data.SolarSensor_solar_irradiance_Avg;
 temp = data.Hygro_Thermo_temperature_Avg;
 windSpeed = data.TopAnemometer_wind_speed_Avg;
-R=1e-12;
-L=2e-2;
-C=550e-6;
+R=0.001;
+L=27e-3;
+C=13.5e-4;
 Lf=(0.1*U^2)/(2*pi*f*P);
 RLf=Lf*100;
 
